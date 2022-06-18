0d5300a1aebf2e6fd37e397459c65e9ba03c5aef
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2022-06-18 19:02:47 +0600

    Time: 587 ms (67.54%), Space: 193.3 MB (26.49%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,49 +1,67 @@
-class trie {
-    trie* ch[27];
-    int weight;
-public:
-    trie() {
-        memset(ch, 0, sizeof(ch));
-        weight = 0;
-    }
-    void insert(string str, int weight) {
-        trie* node = this;
-        for (char c: str) {
-            if (node->ch[c - 'a'] == NULL)
-                node->ch[c - 'a'] = new trie();
-            node = node->ch[c - 'a'];
-            node->weight = weight;
-        }
-    }
-    int startsWith(string str) {
-        trie* node = this;
-        for (char c: str) {
-            if (node->ch[c - 'a'] == NULL)
-                return -1;
-            node = node->ch[c - 'a'];
-        }
-        return node->weight;
-    }
-};
 class WordFilter {
 public:
-    trie root;
+    
+    struct trie{
+        trie *nxt[28];
+        int index;
+        
+        trie(){
+            for(int i=0; i<28; i++){
+                nxt[i] = NULL;
+            }
+            index = 0;
+        }
+    };
+    
+    trie *Trieroot;
+    
+    void insert_trie(trie* root, string str, int idx){
+        
+        for(auto x: str){
+            
+            if(root->nxt[x-'a'] == NULL){
+                root->nxt[x-'a'] = new trie();
+            }
+            
+            root = root -> nxt[x-'a'];
+            root -> index = idx;
+        }
+    }
+    
+    int search(trie* root, string str){
+        for(auto x: str){
+            if(root->nxt[x-'a'] == NULL){
+                return -1;
+            }
+            root = root -> nxt[x-'a'];
+        }
+        return root->index;
+    }
+    
+    
     WordFilter(vector<string>& words) {
+        Trieroot = new trie();
+        
         int idx = 0;
         for (string word: words) {
-            root.insert(word, idx);
+            insert_trie(Trieroot, word, idx);
             string tmp = "{" + word;
             for (int i = word.length() - 1; i >= 0; i--) {
                 string newword = word[i] + tmp;
                 tmp = newword;
-                root.insert(newword, idx);
+                insert_trie(Trieroot, newword, idx);
             }
             idx++;
         }
     }
     
     int f(string prefix, string suffix) {
-        string word = suffix + "{" + prefix;
-        return root.startsWith(word);
+       return search(Trieroot, suffix + "{" + prefix);
     }
 };
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * WordFilter* obj = new WordFilter(words);
+ * int param_1 = obj->f(prefix,suffix);
+ */
