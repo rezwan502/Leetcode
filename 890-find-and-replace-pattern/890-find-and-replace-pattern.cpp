6b3bdc2eb93b443bb87f1e16f017c6796a1f08fe
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2022-07-29 22:47:50 +0600

    Time: 10 ms (28.33%), Space: 8.5 MB (53.91%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -11,14 +11,24 @@ public:
         for(int i=1; i<pattern.length(); i++){
             if(!mp[pattern[i]]) mp[pattern[i]] = ++cnt;
             str += to_string(mp[pattern[i]]) + '-';
-        }
-        
-        //cout << "str: " << str << endl;
-        
+        }    
         return str;
     }
     
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        // Intution: As we can convert char to any char. we just need to find a pattern of distinct char 
+        // words:
+        // abc -> 1-2-3
+        // deq -> 1-2-3
+        // mee -> 1-2-2
+        // aqq -> 1-2-2
+        // dkd -> 1-2-1
+        // ccc -> 1-1-1
+        // pattern:
+        // abb -> 1-2-2
+        
+        // Here we can see that "mee", "aqq" have same pattern as abb.
+        
         string valid = convert_string(pattern);
         vector<string> ans;
         
