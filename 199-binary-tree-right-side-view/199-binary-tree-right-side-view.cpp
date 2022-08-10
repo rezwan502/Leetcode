cbaef9ace02d50f521225481ee0a530fc9753e21
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2022-08-10 23:49:56 +0600

    Time: 2 ms (86.03%), Space: 11.9 MB (66.60%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -14,21 +14,18 @@ public:
     void dfs(TreeNode* root, int level, vector<int>& ans){
         if(!root) return ;
         
-        if(level > ans.size()){
-            //cout << "V: " << root->val << endl;
+        if(level == ans.size()){
             ans.push_back(root->val);
         } 
         
         dfs(root->right,level+1,ans);
         dfs(root->left,level+1,ans);
-
     }
-    
     
     vector<int> rightSideView(TreeNode* root) {
         if(!root) return {};
         vector<int> ans;
-        dfs(root,1,ans);
+        dfs(root,0,ans);
         return ans;
     }
 };
