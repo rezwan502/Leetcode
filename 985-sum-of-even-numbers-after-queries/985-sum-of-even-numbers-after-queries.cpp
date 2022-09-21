7f7481bc1bf097aa5d8566df13c42c78a0e35d3a
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2022-09-22 01:06:36 +0600

    Time: 283 ms (12.30%), Space: 49.9 MB (16.27%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.cpp
@@ -11,22 +11,14 @@ public:
         for (auto x: queries) {
             int val = x[0];
             int idx = x[1];
-            int temp = nums[idx];
-         
+            
+            if (nums[idx] % 2 == 0 ) totalSum -= nums[idx];
+            
             nums[idx] += val;
             
-            if(nums[idx] % 2 == 0) {
-                if ( temp % 2 != 0) {
-                    temp = 0;
-                }
-                totalSum = (totalSum - temp) + nums[idx];
-                ans.push_back(totalSum);
-            } else if (temp % 2 == 0 ) {
-                totalSum = totalSum - temp;
-                ans.push_back(totalSum);
-            } else {
-                ans.push_back(totalSum);
-            }
+            if (nums[idx] % 2 == 0) totalSum += nums[idx];
+            
+            ans.push_back(totalSum);
         }
         
         return ans;
