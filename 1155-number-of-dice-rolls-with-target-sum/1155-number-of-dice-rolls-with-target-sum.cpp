91924df4a07faa6047c7e3eaaeb81e18f00a0906
Md. Rezwanul Islam <rezwan.sec@gmail.com>
2022-10-03 00:40:45 +0600

    Time: 27 ms (79.65%), Space: 6.1 MB (76.82%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -4,13 +4,12 @@ public:
     int MOD = 1e9+7;
     int dp[32][1002];
     
-    int findPossibleWays(int dice, int numbers, int target) {
+    int findPossibleWays(int dice, int numbers, int target, int res = 0) {
         if ( dice == 0 && target == 0) return 1;
         if (dice <= 0 || target <= 0) return 0;
         
         if (dp[dice][target] != -1) return dp[dice][target];
         
-        int res = 0;
         for(int i = 1; i<=numbers; i++) {
             res = (res + findPossibleWays(dice-1,numbers,target-i) % MOD) % MOD;
         }
